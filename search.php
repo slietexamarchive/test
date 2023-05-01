7e9efcbdb73d32a9239690b70f56db8222ed26fa
slietexamarchive <132224020+slietexamarchive@users.noreply.github.com>
2023-05-01 22:32:29 +0530

    Update search.php

--- search.php
+++ search.php
@@ -1,19 +1,15 @@
 <?php
+if (isset($_GET['search'])) {
+    $query = $_GET['search'];
+    $filelist = json_decode(file_get_contents('filelist.json'), true);
 
-// Define the file path of the JSON file containing the file names and links
-$file_path = 'files.json';
-
-// Get the search text from the form data
-$search_text = $_POST['search-text'];
-
-// Load the JSON file containing the file names and links
-$file_list = json_decode(file_get_contents($file_path), true);
-
-// Filter the file list to only include files that match the search text
-$filtered_files = array_filter($file_list, function($file) use ($search_text) {
-    return stripos($file['name'], $search_text) !== false;
-});
-
-// Return the filtered file list as JSON
-header('Content-Type: application/json');
-echo json_encode($filtered_files);
+    foreach ($filelist as $file) {
+        if (strpos($file['name'], $query) !== false) {
+            echo '<tr>';
+            echo '<td>' . $file['name'] . '</td>';
+            echo '<td><a href="' . $file['link'] . '">Download</a></td>';
+            echo '</tr>';
+        }
+    }
+}
+?>
