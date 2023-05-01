3ccc4a1a52f01b9dd5ef294f344af8bb5d3541dd
slietexamarchive <132224020+slietexamarchive@users.noreply.github.com>
2023-05-01 20:48:23 +0530

    Update server.php

--- server.php
+++ server.php
@@ -1,15 +1,19 @@
 <?php
-$dir = './files/';
-$search = isset($_GET['q']) ? trim($_GET['q']) : '';
-$files = [];
 
-if ($search !== '') {
-  foreach (scandir($dir) as $file) {
-    if ($file !== '.' && $file !== '..' && stripos($file, $search) !== false) {
-      $files[] = $file;
-    }
-  }
-}
+// Define the file path of the JSON file containing the file names and links
+$file_path = 'files.json';
 
+// Get the search text from the form data
+$search_text = $_POST['search-text'];
+
+// Load the JSON file containing the file names and links
+$file_list = json_decode(file_get_contents($file_path), true);
+
+// Filter the file list to only include files that match the search text
+$filtered_files = array_filter($file_list, function($file) use ($search_text) {
+    return stripos($file['name'], $search_text) !== false;
+});
+
+// Return the filtered file list as JSON
 header('Content-Type: application/json');
-echo json_encode($files);
+echo json_encode($filtered_files);
