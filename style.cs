3fac255e42d2db78abaa0db2870dab16d10c334c
slietexamarchive <132224020+slietexamarchive@users.noreply.github.com>
2023-05-01 22:48:26 +0530

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,59 @@
+body {
+	font-family: Arial, sans-serif;
+}
+
+h1 {
+	font-size: 2em;
+	margin-bottom: 20px;
+}
+
+form {
+	margin-bottom: 20px;
+}
+
+label {
+	display: inline-block;
+	width: 80px;
+}
+
+input[type="text"] {
+	padding: 5px;
+	font-size: 1em;
+	width: 300px;
+}
+
+button[type="submit"] {
+	padding: 5px;
+	font-size: 1em;
+	background-color: #007bff;
+	color: #fff;
+	border: none;
+	cursor: pointer;
+}
+
+button[type="submit"]:hover {
+	background-color: #0062cc;
+}
+
+.results {
+	margin-top: 20px;
+}
+
+ul {
+	list-style: none;
+	padding: 0;
+	margin: 0;
+}
+
+li {
+	margin-bottom: 10px;
+}
+
+a {
+	color: #007bff;
+	text-decoration: none;
+}
+
+a:hover {
+	text-decoration: underline;
+}
